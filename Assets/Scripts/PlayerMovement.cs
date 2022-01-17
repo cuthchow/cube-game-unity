9bd6945cdeba92076f9ab4c94e0bf9ba5b918f2c
Cuthbert Chow <cuthbert.chow@gmail.com>
2022-01-16 17:39:44 -0800

    Add Obstacle Class and Modified Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public Rigidbody rb; 
+    public Rigidbody rb;
+    public MeshRenderer mr;
+
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f; 
 
     // Start is called before the first frame update
     void Start()
@@ -13,6 +17,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 1200 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime * 5);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
     }
+
 }
